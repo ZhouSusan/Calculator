05b8967534edd962c6c946628e77ad0ae3929ff2
ProwlingLynx <tamezjavier@gmail.com>
2022-10-26 23:00:02 -0500

    Adding performCalculation function.

--- index.js
+++ index.js
@@ -32,4 +32,30 @@ let previousValue = 0;
 let nextValue = 0;
 
 // Function that takes two values and applies the operator function
-// const calculate = (val)
+const calculate = (previous, next, operator) => operator(previous, next);
+
+// This function will mutate previousValue and nextValue by applying the defined functions above it.
+const performCalculation = () => {
+  switch (testSwitch){
+    case "add":
+      // do stuff
+      previousValue = calculate(previousValue, nextValue, additon);
+      break;
+    case "minus":
+      // do stuff;
+      previousValue = calculate(previousValue, nextValue, subtract);
+      break;
+    case "multiply":
+      // do stuff
+      previousValue = calculate(previousValue, nextValue, multiply);
+      break;
+    case "divide":
+      // do stuff
+      previousValue = calculate(previousValue, nextValue, divide);
+      break;
+    default:
+      console.log("Whoopsies");
+      throw new Error("Using illegal operator");
+  };
+}
+
